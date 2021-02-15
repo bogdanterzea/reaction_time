df16174b5377e0630dea66f1550d2a04dcff59b8
Terzea Bogdan <terzeabogdan2007@yahoo.com>
2021-02-15 13:03:04 +0200

    Create start method with a delay

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <h1>Reacton Timer</h1>
+  <button @click="start">PLAY!</button>
 </template>
 
 <script>
-
 export default {
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
   name: 'App',
   components: {
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+    }
   }
 }
 </script>
